4937d3c44b1a35bcfa6bf6cd7aa621388891ad2a
kiryk <alkiryk@wp.pl>
2021-01-20 21:51:40 +0100

    `del dev.img` instead of `del dev.flp`

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 nasm -O0 -f bin -o bootloader.sys bootloader.asm
 nasm -O0 -f bin -o kernel.sys kernel.asm
 nasm -O0 -f bin -o program.prg program.asm
+nasm -O0 -f bin -o echo.prg echo.asm
 
-del dev.flp
+del dev.img
 copy bootloader.sys dev.img
 
 rem 1474560B = 1440K
@@ -11,5 +12,6 @@ fsutil file seteof dev.img 1474560
 imdisk -a -f dev.img -s 1440K -m B:
 copy kernel.sys B:\
 copy program.prg B:\
+copy echo.prg B:\
 copy readme.md B:\
 imdisk -D -m B:
