d78b8be9c32392102aed9d19649efc91720bae04
kiryk <alkiryk@wp.pl>
2021-01-22 23:40:54 +0100

    untested fs_rename_file routine

--- fs.asm
+++ fs.asm
@@ -950,6 +950,58 @@ fs_remove_file:
 	ret
 
 
+fs_rename_file:
+; IN:  ds:si: original filename pointer
+; IN:  es:di: new filename pointer
+;
+; OUT: the desired file is renamed
+; OUT: changes are written to the disk
+;
+; VARS:
+	.size equ fs_dir_entry.namesize
+	.tag  equ 0
+
+	pusha
+	sub sp, .size
+	mov bp, sp
+
+	mov dx, os_kernel_base      ; The segment we will use for di and si
+	mov bx, si                  ; Save original si
+	mov cx, di                  ; Save original di
+
+	mov si, cx
+	call fs_find_file           ; If the new filename belongs to an
+	jnc short .error            ; existing file, return with error
+
+	mov es, dx                  ; Translate the filename to tag
+	mov di, bp
+	call fs_filename_to_tag
+	jc short .error             ; Is the filename correct?
+
+	mov si, bx
+	call fs_find_file
+	jc short .error             ; Does the file we want to rename exist?
+
+	; Now di contains the file's directory entry
+
+	mov cx, fs_dir_entry.namesize
+	mov ds, dx
+	mov si, bp
+	rep movsb                   ; Insert the new tag to the file data
+
+	call fs_write_root
+.success:
+	add sp, fs_dir_entry.namesize
+	clc
+	jmp short .return
+
+.error:
+	add sp, fs_dir_entry.namesize
+	stc
+.return:
+	popa
+
+
 fs_read_file:
 ; IN:  ax:    first physical sector
 ; IN:  es:bx: buffer to load the file to
